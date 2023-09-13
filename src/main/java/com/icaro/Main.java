7ae9a1b54d38a0acef61da2cf3613c916844553d
Anbelrf <anbelmaririvas@gmail.com>
2023-09-13 16:43:03 -0300

    Tipo de datos

--- src/main/java/com/icaro/Main.java
+++ src/main/java/com/icaro/Main.java
@@ -3,7 +3,7 @@ package com.icaro;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!\n");
+       System.out.printf("Hello and welcome!\n");
 
         System.out.printf("Hello naturaleza\n");
 
@@ -18,5 +18,45 @@ public class Main {
         System.out.println("verdadero");
 
 
-    }
+
+
+        // declaracion e instanciacion
+        int edad; // declarando la variable edad de tipo int
+        char fecha;
+        edad = 18; // inicializando la variable edad con valor 18
+        char tipo_documento = 'D' ; // declarar e inicializar una variable
+
+        // tipos primitivos
+        byte var1 = 5;
+        short var2 = 100;
+        int var3 = 10000;
+        long var4 = 10000000000L;
+
+        float var5 = 10.12f;
+        double var6 = 10.222222222222222;
+
+        char var7 = 't';
+
+        boolean var8 = true;
+        boolean var9 = false;
+
+        String var10 = "hola";
+
+        int valor1 = 10;
+        int valor2 = 20;
+
+        int suma= valor1 + valor2;
+        System.out.println(suma);
+
+        System.out.println(var5 + var6);
+
+        System.out.println(var3 + var5);
+
+
+
+
+
+
+
+          }
 }
