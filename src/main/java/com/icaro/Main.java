742a6e4034e07696228ec5e4f8c80d9ac6cfbac2
Anbelrf <anbelmaririvas@gmail.com>
2023-09-13 01:43:59 -0300

    Proyecto Base

---
+++ src/main/java/com/icaro/Main.java
@@ -0,0 +1,22 @@
+package com.icaro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!\n");
+
+        System.out.printf("Hello naturaleza\n");
+
+        System.out.println(1234);
+
+        System.out.println(123.456);
+
+        System.out.println('c');
+
+        System.out.println("true");
+
+        System.out.println("verdadero");
+
+
+    }
+}
